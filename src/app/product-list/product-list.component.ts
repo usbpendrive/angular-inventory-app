359a4ab70b71c384fd01603ae8baf1f1e607452f
Fernando <ferry.fernando@gmail.com>
2018-11-04 09:37:50 +0700

    Product list component.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Product} from '../product.model';
 
 @Component({
@@ -6,13 +6,25 @@ import {Product} from '../product.model';
   selector: 'product-list',
   templateUrl: './product-list.component.html'
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   @Input() productList: Product[];
   @Output() onProductSelected: EventEmitter<Product>;
 
-  constructor() { }
+  private currentProduct: Product;
 
-  ngOnInit() {
+  constructor() {
+    this.onProductSelected = new EventEmitter();
   }
 
+  clicked(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product: Product): boolean {
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
+  }
 }
