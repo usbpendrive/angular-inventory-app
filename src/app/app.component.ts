1dee106deefa9e50e01e58775b008524cd486f5a
Fernando <ferry.fernando@gmail.com>
2018-11-04 09:07:28 +0700

    Renaming root component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  // tslint:disable-next-line
+  selector: 'inventory-app-root',
+  template: `
+    <div class="inventory-app">
+      (Product placeholder)
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
