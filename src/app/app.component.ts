a47f40c92860ef06272f5b50616e4307f73bef5f
Fernando <ferry.fernando@gmail.com>
2018-11-04 09:16:13 +0700

    Basic product list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Product} from './product.model';
 
 @Component({
   // tslint:disable-next-line
   selector: 'inventory-app-root',
   template: `
     <div class="inventory-app">
-      (Product placeholder)
+      <h1>{{ product.name }}</h1>
+      <span>{{ product.sku }}</span>
     </div>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-inventory-app';
+  product: Product;
+
+  constructor() {
+    this.product = new Product(
+      'ARTEZA',
+      'Arteza Real Brush Pens, 48 Colors for Watercolor Painting',
+      '/assets/images/products/arteza-real-brush-pens.jpg',
+      ['Painting', 'Brush', 'Stationery'],
+      22.6
+    );
+  }
 }
