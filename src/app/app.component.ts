4aa117df5512d9cdfe2b59465c66f853ee2544b7
Fernando <ferry.fernando@gmail.com>
2018-11-04 09:26:09 +0700

    Dummy products.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,37 @@ import {Product} from './product.model';
 @Component({
   // tslint:disable-next-line
   selector: 'inventory-app-root',
-  template: `
-    <div class="inventory-app">
-      <h1>{{ product.name }}</h1>
-      <span>{{ product.sku }}</span>
-    </div>
-  `,
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  product: Product;
+  product: Product[];
 
   constructor() {
-    this.product = new Product(
+    this.product = [
+      new Product(
       'ARTEZA',
       'Arteza Real Brush Pens, 48 Colors for Watercolor Painting',
       '/assets/images/products/arteza-real-brush-pens.jpg',
       ['Painting', 'Brush', 'Stationery'],
-      22.6
-    );
+      22.6),
+      new Product(
+        'WATERCOLOR',
+        'Watercolor Brush Pens,GUOfeudallord 20 Color Paint Brush Markers',
+        '/assets/images/products/watercolor-brush-pen.jpg',
+        ['Painting', 'Brush', 'Art'],
+        15.99
+      ),
+      new Product(
+        'GC100',
+        'GC 100 Dual Tip Brush Pen Marker Set Flexible Brush & Fineliner Tips',
+        '/assets/images/products/gc-100-dual-tip-brush-pen.jpg',
+        ['Art', 'Supplies', 'Refills'],
+        35.99
+      )
+    ];
+  }
+
+  productWasSelected(product: Product) {
+    console.log('Product clicked', product);
   }
 }
